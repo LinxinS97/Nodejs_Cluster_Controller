b561526b2adafa5b0482f831934e61b1bde8a329
unknown <songlinxin@cn.net.ntes>
2018-10-09 14:40:46 +0800

    搭建网关

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const proxyServer = require('http-proxy');
 const servers = require('./config');
-const port = 80;
+const port = parseInt(process.argv[2]);
+
+// 创建代理服务器，用于分发请求
+require('http').createServer(require('proxy-by-directory')(servers)).listen(port);
+console.log(`listening on port ${process.argv[2]}`);
