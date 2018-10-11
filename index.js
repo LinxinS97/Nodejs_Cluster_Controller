a8f30cfe7a8a4e7615c7e744a89f3a94d2d1d92c
unknown <songlinxin@cn.net.ntes>
2018-10-11 18:16:31 +0800

    增加负载均衡模式

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const proxyServer = require('http-proxy');
 const servers = require('./config');
+const httpProxy = require('http-proxy');
 const port = parseInt(process.argv[2]);
+const mode = process.argv[3];
 
-// 创建代理服务器，用于分发请求
-require('http').createServer(require('proxy-by-directory')(servers)).listen(port);
+/**
+ * 集群控制器
+ */
+let temp = servers.balance.concat();
+
+switch(mode) {
+    /**
+     * 网关模式，根据路径分发请求
+     */
+    case 'gateway': 
+        console.log('gateway mode on');
+        require('http').createServer(require('proxy-by-directory')(servers.gateway)).listen(port);
+        break;
+    /**
+     * 负载均衡模式，根据服务器集群循环分发请求
+     */
+    case 'balance':
+        const proxy = httpProxy.createProxyServer({});
+        console.log('balance mode on');
+        require('http').createServer((req, res) => {
+            let url = temp.shift();
+            console.log('redirected to ' + url);
+            proxy.web(req, res, { target: url });
+            temp.push(url);
+        }).listen(port);
+        break;
+}
 console.log(`listening on port ${process.argv[2]}`);
